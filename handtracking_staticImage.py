d04da16aad647164adc97765f2e1c553621dd1c1
unknown <dbwp031@khu.ac.kr>
2022-09-30 21:36:41 +0900

    vector

--- handtracking_staticImage.py
+++ handtracking_staticImage.py
@@ -4,8 +4,13 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For static images:
-IMAGE_FILES = [r'C:\Users\37739\Documents\khuthon_2022\HandTracking\static_image\hand_static_image.jpg']
+Wr = 0
+M0 = 9
+M3 = 12
+
+IMAGE_FILES = [r'C:\Users\37739\Documents\khuthon_2022\HandTracking\static_images\left.jpg',]
+              # r'C:\Users\37739\Documents\khuthon_2022\HandTracking\static_images\right.jpg',
+              # r'C:\Users\37739\Documents\khuthon_2022\HandTracking\static_images\straight.jpg']
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=1,
@@ -25,11 +30,27 @@ with mp_hands.Hands(
     annotated_image = image.copy()
     for hand_landmarks in results.multi_hand_landmarks:
       print('hand_landmarks:', hand_landmarks)
+      # print(len(hand_landmarks))
       print(
           f'Index finger tip coordinates: (',
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
       )
+      # with open(r'C:\Users\37739\Documents\khuthon_2022\HandTracking\static_landmarks\left.txt','a') as f:
+      #   for idx, landmark in enumerate(hand_landmarks.landmark):
+      #     # print(idx,landmark)
+      #     f.write(f"{idx}\n")
+      #     f.write(f"{landmark}\n")
+
+      # print(f"0,{hand_landmarks.landmark[0].x}")
+      
+      Wr2M0 = [hand_landmarks.landmark[M0].x-hand_landmarks.landmark[Wr].x,
+              hand_landmarks.landmark[M0].y-hand_landmarks.landmark[Wr].y,
+              hand_landmarks.landmark[M0].z-hand_landmarks.landmark[Wr].z]
+      Wr2M3 = [hand_landmarks.landmark[M3].x-hand_landmarks.landmark[Wr].x,
+              hand_landmarks.landmark[M3].y-hand_landmarks.landmark[Wr].y,
+              hand_landmarks.landmark[M3].z-hand_landmarks.landmark[Wr].z]
+      print("vector",Wr2M0)
       mp_drawing.draw_landmarks(
           annotated_image,
           hand_landmarks,
